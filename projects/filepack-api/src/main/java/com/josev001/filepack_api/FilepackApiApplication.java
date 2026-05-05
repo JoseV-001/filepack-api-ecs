54a01361b9fa8755329c9cce0b4f6dd8d7a1e502
JoseV-001 <Leaphaz@gmail.com>
2026-05-04 23:17:45 -0300

    first commit

---
+++ projects/filepack-api/src/main/java/com/josev001/filepack_api/FilepackApiApplication.java
@@ -0,0 +1,13 @@
+package com.josev001.filepack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilepackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilepackApiApplication.class, args);
+	}
+
+}
